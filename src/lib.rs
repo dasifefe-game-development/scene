e830e8c4c9792386564faecc8f53901fc21c265c
Felipe Ferreira da Silva <dasifefe@gmail.com>
2020-04-03 08:26:59 -0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+use string_fixed::StringFixed;
+
+pub trait TraitSceneData {
+    fn initialize(&mut self);
+    fn activate(&mut self);
+    fn deactivate(&mut self);
+    fn update(&mut self);
+    fn render(&mut self);
+    fn audio(&mut self);
+}
+
+pub struct Scene<D: TraitSceneData> {
+    name: StringFixed,
+    pub data: D,
+}
+
+impl<D: TraitSceneData> Scene<D> {
+    pub fn initialize(&mut self, name: &str) {
+        self.name.from(name);
+        self.data.initialize();
+    }
+    pub fn activate(&mut self) {
+        self.data.activate();
+    }
+    pub fn deactivate(&mut self) {
+        self.data.deactivate();
+    }
+    pub fn update(&mut self) {
+        self.data.update();
+    }
+    pub fn render(&mut self) {
+        self.data.render();
+    }
+    pub fn audio(&mut self) {
+        self.data.audio();
+    }
+}
